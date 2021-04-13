443b5a410bb15d0dc27a6170a9978e81b531f0f5
taku824 <augusttaku824@gmail.com>
2021-04-13 16:20:09 +0000

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,23 @@
+class BooksController < ApplicationController
+  
+  def index 
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def destroy
+  end
+  
+  def create
+  end
+  
+  def update
+  end
+  
+  
+  
+end
