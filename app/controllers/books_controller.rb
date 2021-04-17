3f6198778ec4e33e1db3f028bdc942d88ec7f6ff
taku824 <augusttaku824@gmail.com>
2021-04-17 07:49:52 +0000

    各機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,68 @@
 class BooksController < ApplicationController
-  
-  def index 
+
+  def new
+
   end
-  
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
   def show
+    @book = Book.find(params[:id])
   end
-  
+
   def edit
+    @book = Book.find(params[:id])
   end
+
   
+
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:success] = "Book was successfully destroyed."
+    redirect_to books_path
   end
-  
+
+
+
   def create
+    
+    book = Book.new(book_params)
+    if book.save
+      flash[:success] = "Book was successfully created."
+      redirect_to "/books/#{book.id}"
+    else
+      flash.now[:danger] = "Failed to save."
+      render "/books"
+    end
   end
   
+  
+  
+  
+
   def update
+    book = Book.find(params[:id])
+    
+    
+    if book.update(book_params)
+      flash[:success] = "Book was successfully updated."
+      redirect_to "/books/#{book.id}"
+    else
+      flash.now[:danger] = "Failed to update."
+      render "/books"
+    end
   end
-  
-  
-  
+
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '' => 'homes#top'
   get 'books' => 'books#index'
-
-
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  get 'books/:id/update' => 'books#update'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 
 
 end
