84b7d7e936aab59e8dc4289537ddaf704b91f5cf
taku824 <augusttaku824@gmail.com>
2021-04-20 15:06:18 +0000

    RSpecテストの実装

--- spec/factories/book.rb
+++ spec/factories/book.rb
@@ -1,6 +1,13 @@
+# FactoryBot.define do
+#   factory :book do
+#     sequence(:title) { |n| "title#{n}"}
+#     sequence(:body) { |n| "body#{n}"}
+#   end
+# end
+
 FactoryBot.define do
   factory :book do
-    title { 'タイトル' }
-  body { '本文' }
+    title { Faker::Lorem.characters(number:5) }
+    body { Faker::Lorem.characters(number:20) }
   end
 end
